615c43c400821c134c5d053a53d0e2f374eee311
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-02-05 20:34:24 -0300

    Refactor Division component to accept a title prop

--- src/components/Division.tsx
+++ src/components/Division.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Division: React.FC = () => {
+interface DivisionProps {
+    title: string;
+}
+
+const Division: React.FC<DivisionProps> = ({ title }) => {
     return (
         <div className="w-full h-14 border-t border-blue-700 justify-start items-start inline-flex">
             <div className="pt-2.5 justify-start items-start flex">
-                <div className="w-full text-xl lg:text-2xl font-normal ">Research</div>
+                <div className="w-full text-xs font-normal ">{title}</div>
             </div>
         </div>
     );
