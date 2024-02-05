1787783690af3d9b249be0cd9b71ddff18f6ed10
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-02-05 20:34:34 -0300

    Update SubTitle and Division components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,10 +15,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <SubTitle title="Impartiality in Machine Learning with Application in Medical Images"/>
+      <SubTitle title="Impartiality in Machine Learning with Application in Medical Images."/>
       <Title title="FAIRNESS"/>
       <About />
-      <Division />
+      <Division title='Research'/>
       <Blog />
     </div>
   );
