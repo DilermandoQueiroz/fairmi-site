6f99740e689c79bef1f94ec6132b28e4015a9ea5
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-02-07 14:02:16 -0300

    Refactor TextBlog component styling

--- src/components/TextBlog.tsx
+++ src/components/TextBlog.tsx
@@ -6,8 +6,9 @@ interface TitleProps {
 
 const TextBlog: React.FC<TitleProps> = ({ text }) => {
     return (
-        <div className="w-full pl-60 pr-72 pb-14 flex-col justify-center items-start inline-flex">
-            <div className="self-stretch text-base font-semibold ">{text}</div>
+        <div className="w-full px-4 sm:pl-28 md:pl-60 md:pr-48 lg:pl-60 lg:pr-60 flex flex-col justify-center items-start">
+            <div className="self-stretch text-base"
+                dangerouslySetInnerHTML={{ __html: text }} />
         </div>
     );
 };
