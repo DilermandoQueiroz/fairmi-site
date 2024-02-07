c5739cac5ef1c4513ac063f5f509c3cd70e65922
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-02-07 14:02:43 -0300

    Add dynamic rendering of blog posts

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,8 +4,22 @@ import Title from '@/components/Title';
 import SubTitle from '@/components/SubTitle';
 import Division from '@/components/Division';
 import Blog from '@/components/Blog';
+import BlogCard from '@/components/BlogCard';
+import { GetStaticProps } from 'next';
+import { getAllPosts } from '@/lib/api';
+import Link from 'next/link';
 
-export default function BlogHome() {
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = getAllPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default function BlogHome({ posts }: { posts: any[] }) { // Use TypeScript to define the props type
   return (
     <div>
       <Head>
@@ -13,12 +27,21 @@ export default function BlogHome() {
         <meta name="description" content="Welcome to FairMI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <Header />
-        <SubTitle title="We are developing adjustable ML models for better performance and fairness."/>
-        <Title title="BLOG"/>
-        <Division title='Research'/>
-        <Blog />
-        <Blog />
-      </div>
+      <Header />
+      <SubTitle title="We are developing adjustable ML models for better performance and fairness."/>
+      <Title title="BLOG"/>
+      <Division title='Research'/>
+      <Blog>
+        {posts.map((post, index) => (
+          <Link href={`/blog/${post.slug}`} passHref>
+            <BlogCard 
+              key={index}
+              title={post.title}
+              imageUrl={post.image}
+              date={post.date}/>
+          </Link>
+        ))}
+      </Blog>
+    </div>
   );
 }
