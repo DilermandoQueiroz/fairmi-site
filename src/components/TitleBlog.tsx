da7980b87374e17f053e2206e8d9b1e822b13e0b
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-02-07 14:02:19 -0300

    Refactor TitleBlog component styles

--- src/components/TitleBlog.tsx
+++ src/components/TitleBlog.tsx
@@ -8,10 +8,10 @@ interface TitleProps {
 
 const TitleBlog: React.FC<TitleProps> = ({ title, date, author }) => {
     return (
-        <div className="w-full pl-60 py-36 pb-14 flex-col justify-center items-start inline-flex">
-            <div className="self-stretch text-5xl font-semibold ">{title}</div>
-            <div className="self-stretch text-xs font-semibold ">{date}</div>
-            <div className="self-stretch text-xs font-semibold ">{author}</div>
+        <div className="w-full px-4 sm:pl-28 md:pl-60 lg:pl-60 py-12 lg:py-36 flex flex-col justify-center items-start">
+            <div className="self-stretch text-2xl md:text-3xl lg:text-5xl font-semibold">{title}</div>
+            <div className="self-stretch text-xs font-semibold mt-2">{date}</div>
+            <div className="self-stretch text-xs font-semibold">{author}</div>
         </div>
     );
 };
