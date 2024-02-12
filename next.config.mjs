e9af2e47913e1c52e53c838aad69bf621055873c
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-02-12 02:34:47 -0300

    Update next.config.mjs for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: "export",
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/fairmi",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true,
 };
+
+module.exports = {
+  // Use the basePath to set the subpath for GitHub Pages
+  basePath: '/fairmi-site',
+  // Enable static HTML export
+  output: 'export',
+};
+
 
 export default nextConfig;
