b8f20b8352acd1e1e45944b54b5dfad146292aeb
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-29 20:28:54 -0300

    Update Title component styling for better responsiveness

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ const Title = ({ title }: { title: string }) => {
   return (
     <div className="flex justify-center items-center h-[300px] pb-[100px]">
       {/* <div className="text-center text-8xl md:text-[10rem] xl:text-[14rem] font-black"> */}
-      <div className="text-center sm: text-[10rem] text-[14rem] font-black">
+      <div className="text-center text-8xl md:text-[10rem] xl:text-[14rem] font-black">
         {title}
       </div>
     </div>
