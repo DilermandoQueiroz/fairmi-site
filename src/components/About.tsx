27b9d56d23f31c005fefe007d6368cb0f4ae21a0
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-29 17:56:54 -0300

    Update font sizes and styles in About and Title components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const About: React.FC = () => {
     return (
-        <div className="py-24 flex justify-center items-center w-full">
-            <div className="flex items-start justify-start flex-grow">
-                <div className="text-xl lg:text-2xl font-normal">
-                We are developing adjustable ML models for better performance and fairness, quantifying impartiality limits, and creating systems that integrate human decision-making fairly. Our goal is to improve safety and set new standards for responsible AI in healthcare.
+        <div className="py-24 flex justify-center items-center w-full font-light">
+            <div className="flex items-start justify-start flex-grow ">
+                <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+                    We are developing adjustable ML models for better performance and fairness, quantifying impartiality limits, and creating systems that integrate human decision-making fairly. Our goal is to improve safety and set new standards for responsible AI in healthcare.
                 </div>
             </div>
         </div>
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Title = ({ title }: { title: string }) => {
   return (
-    <div className="flex justify-center items-center h-[255px] pb-[100px]">
-      <div className="text-center text-7xl sm:text-8xl md:text-9xl lg:text-[10rem] xl:text-[12rem] font-black">
+    <div className="flex justify-center items-center h-[300px] pb-[100px]">
+      <div className="text-center text-8xl md:text-[10rem] xl:text-[14rem] font-black">
         {title}
       </div>
     </div>
