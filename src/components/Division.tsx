554ebd70c973e98ebcf66ac579e5489aee3c53f3
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-29 18:03:32 -0300

    Update Division component font size and style

--- src/components/Division.tsx
+++ src/components/Division.tsx
@@ -6,9 +6,9 @@ interface DivisionProps {
 
 const Division: React.FC<DivisionProps> = ({ title }) => {
     return (
-        <div className="w-full h-14 border-t border-blue-700 justify-start items-start inline-flex">
+        <div className="w-full h-14 border-t border-blue-700 justify-start items-start inline-flex font-extralight">
             <div className="pt-2.5 justify-start items-start flex">
-                <div className="w-full text-xs font-normal ">{title}</div>
+                <div className="w-full text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">{title}</div>
             </div>
         </div>
     );
