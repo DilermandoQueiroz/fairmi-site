f3aba40afa3911aeddb97431d84c780962902673
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-29 20:43:52 -0300

    Update TitleBlog component to use smaller font size for the title on mobile devices

--- src/components/TitleBlog.tsx
+++ src/components/TitleBlog.tsx
@@ -11,7 +11,7 @@ const TitleBlog: React.FC<TitleProps> = ({ title, date, author, paperLink }) => 
     return (
         <div className="flex flex-col justify-center items-center w-full h-full pb-[80px]">
             <div className="text-center pb-2 text-xl font-light">{date}</div>
-            <div className="text-center text-6xl max-w-5xl pb-6">{title}</div>
+            <div className="text-center text-4xl md:text-6xl max-w-5xl pb-6">{title}</div>
             <div className="text-center pb-12 max-w-2xl text-2xl">{author}</div>
             {paperLink && (
                 <div className="text-center">
