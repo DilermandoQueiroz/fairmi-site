e02900f79f1cda4db7660593ae24b62a34519d6b
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-29 20:24:45 -0300

    chore: Refactor FootNote component to use semantic HTML and improve accessibility

--- src/components/FootNote.tsx
+++ src/components/FootNote.tsx
@@ -4,23 +4,17 @@ import React from 'react';
 
 const FootNote: React.FC = () => {
     return (
-        <div className="w-full h-12 lg:px-24 md:px-12 px-6 bg-blue-700 flex-col justify-start items-start gap-7 inline-flex">
-            <div className="self-stretch grow shrink basis-0 justify-start items-center gap-2.5 inline-flex">
-                <div className="grow shrink basis-0 h-7 justify-start items-start flex">
-                    <div className="text-white text-2xl font-normal ">🏥FairMI</div>
-                </div>
-                <div className="grow shrink basis-0 h-8 justify-end items-center flex">
-                    <a href="https://github.com/project-fairmi">
-                        <Image
-                            className='w-8 h-8'
-                            priority
-                            src={GitHub}
-                            alt="Follow us on GitHub"
-                        />
-                    </a>
-                </div>
-            </div>
-        </div>
+        <footer className="w-full bg-blue-700 flex justify-between items-center px-6 md:px-12 lg:px-24 h-12">
+            <div className="text-white text-2xl font-normal">🏥 FairMI</div>
+            <a href="https://github.com/project-fairmi" aria-label="GitHub">
+                <Image
+                    className='w-8 h-8'
+                    priority
+                    src={GitHub}
+                    alt="Follow us on GitHub"
+                />
+            </a>
+        </footer>
     );
 };
 
