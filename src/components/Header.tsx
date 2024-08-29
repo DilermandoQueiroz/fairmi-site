04bd620b1f4a05f7b1f0b8c5b1189d32b1ae049d
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-29 20:19:26 -0300

    Update header component styling and navigation links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,17 @@ const Header = () => {
     <div className="flex justify-center items-center h-14 px-8">
       <div className="flex justify-center items-center gap-12">
         <Link href="/" passHref>
-          <div className="text-base text-xl cursor-pointer transform transition-transform duration-300 hover:-translate-y-1">
+          <div className="text-xl cursor-pointer transform transition-transform duration-300 hover:-translate-y-1">
             home
           </div>
         </Link>
         <Link href="/blog" passHref>
-          <div className="text-base text-xl cursor-pointer transform transition-transform duration-300 hover:-translate-y-1">
+          <div className="text-xl cursor-pointer transform transition-transform duration-300 hover:-translate-y-1">
             blog
           </div>
         </Link>
         <Link href="/team" passHref>
-          <div className="text-base text-xl cursor-pointer transform transition-transform duration-300 hover:-translate-y-1">
+          <div className="text-xl cursor-pointer transform transition-transform duration-300 hover:-translate-y-1">
             our team
           </div>
         </Link>
