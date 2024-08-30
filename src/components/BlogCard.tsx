6ad3b2687b9cc12608f5c628897d4ca99d1bcd42
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-30 17:06:07 -0300

    Update BlogCard component to use responsive layout and object positioning

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -8,11 +8,11 @@ interface BlogCardProps {
 
 const BlogCard: React.FC<BlogCardProps> = ({ imageUrl, title, date }) => {
     return (
-        <div className="w-full max-w-sm rounded-lg overflow-hidden shadow-lg flex flex-col transform transition-transform duration-300">
-            <div className="w-full overflow-hidden flex justify-center items-center">
-                <img className="w-full h-auto max-w-full max-h-80 object-cover transition-transform duration-300 transform hover:scale-110" src={imageUrl} alt={title} />
+        <div className="w-80 h-96 rounded-lg overflow-hidden shadow-lg flex flex-col transform transition-transform duration-300">
+            <div className="w-full h-2/3 overflow-hidden flex justify-center items-center">
+                <img className="w-full h-full object-cover transition-transform duration-300 transform hover:scale-110" src={imageUrl} alt={title} />
             </div>
-            <div className="p-4 flex flex-col justify-center items-start">
+            <div className="p-4 flex flex-col justify-center items-start h-1/2">
                 <div className="text-lg font-semibold mb-1">{title}</div>
                 <div className="text-sm font-normal">{date}</div>
             </div>
