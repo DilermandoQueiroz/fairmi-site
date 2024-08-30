b5f392993cb88e4a0fc2794424caf207a8025bb5
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-30 17:09:23 -0300

    Update Blog component to remove unnecessary width classes

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Blog = ({ children }: { children: React.ReactNode }) => {
     // Wrap each child in a div that controls its maximum width and responsiveness
     const wrappedChildren = React.Children.map(children, child => (
-        <div className="w-full sm:w-1/2 md:w-1/3 p-4">
+        <div className="p-4">
             {child}
         </div>
     ));
