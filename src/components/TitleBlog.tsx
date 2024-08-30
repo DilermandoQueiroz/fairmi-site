8cc4d43e75a8bf6b64944abfbaea5384d1f1d588
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-29 21:33:48 -0300

    Update TitleBlog component font sizes for better responsiveness

--- src/components/TitleBlog.tsx
+++ src/components/TitleBlog.tsx
@@ -10,9 +10,9 @@ interface TitleProps {
 const TitleBlog: React.FC<TitleProps> = ({ title, date, author, paperLink }) => {
     return (
         <div className="flex flex-col justify-center items-center w-full h-full pb-[80px]">
-            <div className="text-center pb-2 text-xl font-light">{date}</div>
+            <div className="text-center pb-2 text-lg md:text-xl font-light">{date}</div>
             <div className="text-center text-4xl md:text-6xl max-w-5xl pb-6">{title}</div>
-            <div className="text-center pb-12 max-w-2xl text-2xl">{author}</div>
+            <div className="text-center pb-12 max-w-2xl text-xl md:text-2xl">{author}</div>
             {paperLink && (
                 <div className="text-center">
                     <a href={paperLink}>
