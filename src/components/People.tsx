240000118a4831b66468f0e6b2c46dfe27a77f8d
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-30 15:25:28 -0300

    Update PeopleCard component to include institution in position

--- src/components/People.tsx
+++ src/components/People.tsx
@@ -2,34 +2,39 @@ import React from 'react';
 
 const People: React.FC = () => {
     const people = [
-        { name: "Anderson Carlos", position: "BSc Student" },
-        { name: "André Anjos", position: "Coordenador" },
-        { name: "Bruno Pires", position: "BSc Student" },
-        { name: "Dilermando Queiroz Neto", position: "PhD Student" },
-        { name: "Gabriel Dias Dadalto", position: "" },
-        { name: "Giuliana Prado Peixoto", position: "" },
-        { name: "Gökhan Özbulak", position: "PhD Student" },
-        { name: "Isabela Amaro", position: "MSc Student" },
-        { name: "Jadson", position: "" },
-        { name: "Lilian Berton", position: "Coordenador" },
-        { name: "Maíra Fatoretto", position: "Postdoc Student" },
-        { name: "Savio Mendes", position: "" },
-        { name: "Oscar Jimenez", position: "Postdoc Student" }
+        { name: "Anderson Carlos", position: "BSc Student", institution: "Unifesp" },
+        { name: "André Anjos", position: "Coordinator", institution: "Unifesp" },
+        { name: "Bruno Pires", position: "BSc Student", institution: "Unifesp" },
+        { name: "Dilermando Queiroz Neto", position: "PhD Student", institution: "Idiap" },
+        { name: "Gabriel Dias Dadalto", position: "Master Student", institution: "Unifesp" },
+        { name: "Giuliana Prado Peixoto", position: "BSc Student", institution: "Unifesp" },
+        { name: "Gökhan Özbulak", position: "PhD Student", institution: "Idiap" },
+        { name: "Isabela Amaro", position: "MSc Student", institution: "Unifesp" },
+        { name: "Lilian Berton", position: "Coordinator", institution: "Unifesp" },
+        { name: "Maíra Fatoretto", position: "Postdoc Student", institution: "Unifesp" },
+        { name: "Savio Mendes", position: "BSc Student", institution: "Unifesp" },
+        { name: "Oscar Jimenez", position: "Postdoc Student", institution: "Idiap" },
+        { name: "Marcelo Tatit Sapienza", position: "Collaborator", institution: "FM-USP" },
+        { name: "Carlos Alberto Buchpiguel", position: "Collaborator", institution: "FM-USP" },
+        { name: "Anete Trajman", position: "Collaborator", institution: "FM-UFRJ" }
     ];
 
     const positionPriority: { [key: string]: number } = {
-        "Coordenador": 1,
-        "Postdoc Student": 2,
-        "PhD Student": 3,
-        "MSc Student": 4,
-        "BSc Student": 5,
-        "": 6 // For positions that are empty
+        "Coordinator": 1,
+        "Collaborator": 2,
+        "Postdoc Student": 3,
+        "PhD Student": 4,
+        "MSc Student": 5,
+        "Master Student": 6,
+        "BSc Student": 7,
+        "Undergraduate Student": 8,
+        "": 9 // For positions that are empty
     };
 
     const sortedPeople = people.sort((a, b) => {
         const priorityA = positionPriority[a.position] || 100;
         const priorityB = positionPriority[b.position] || 100;
-        
+
         if (priorityA !== priorityB) {
             return priorityA - priorityB;
         }
@@ -39,7 +44,11 @@ const People: React.FC = () => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full pb-[50px]">
             {sortedPeople.map((person) => (
-                <PeopleCard key={person.name} name={person.name} position={person.position} />
+                <PeopleCard 
+                    key={person.name} 
+                    name={person.name} 
+                    position={`${person.position} from ${person.institution}`} 
+                />
             ))}
         </div>
     );
