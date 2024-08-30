a9deccf0293a221b11bf3f2a34d83a53d1c1bebe
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-08-30 17:10:29 -0300

    chore: Update Blog component to include bottom padding for children

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Blog = ({ children }: { children: React.ReactNode }) => {
     // Wrap each child in a div that controls its maximum width and responsiveness
     const wrappedChildren = React.Children.map(children, child => (
-        <div className="p-4">
+        <div className="p-4 pb-[100px]">
             {child}
         </div>
     ));
