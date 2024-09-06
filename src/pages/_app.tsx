3901974495a904245ecaf59d0fd83198ef6b3883
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-09-06 09:12:50 -0300

    Update _app.tsx to include @vercel/speed-insights and @vercel/analytics dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />;
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
