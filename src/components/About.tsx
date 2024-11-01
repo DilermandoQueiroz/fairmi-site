96d8eb13d610a50857b15828b02390a63380d51f
Dilermando Queiroz <52236585+DilermandoQueiroz@users.noreply.github.com>
2024-11-01 16:29:26 -0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About: React.FC = () => {
             <div className="flex items-start justify-start flex-grow ">
                 {/* <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl"> */}
                 <div className="text-4xl xl:text-6xl">
-                    We are developing adjustable Machine Learning (ML) models for better performance and fairness, quantifying impartiality limits, and creating systems that integrate human decision-making fairly. Our goal is to improve safety and set new standards for responsible Artificial Intelligence (AI) in healthcare.
+                    We are developing adaptable Artificial Intelligence (AI) models for medical image analysis to enhance performance and fairness, measure impartiality, and integrate human decision-making. Our goal is to improve safety and set new standards for responsible AI in healthcare.
                 </div>
             </div>
         </div>
